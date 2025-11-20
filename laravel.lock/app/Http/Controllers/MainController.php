b1e471d46a47793cf1552de00e47476bf4a5dbeb
ShadowWEBRAS <egort011t@gmail.com>
2025-11-20 15:22:28 +0700

    lesson 5

---
+++ laravel.lock/app/Http/Controllers/MainController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\View\View;
+
+class MainController extends Controller {
+	public function index(): View {
+		return view('hi', ['title' => 'Main page']);
+	}
+
+	public function test() {
+		return 'test page';
+	}
+}
